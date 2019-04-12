44ac937e53c71a0b3d3c894f3a7c3f00fe6219a2
shannon7777 <shannonsimoncherry@gmail.com>
2019-04-12 15:54:22 +0800

    - added user,trips,tripevents & file attachments
    Signed-off-by: shannon7777 <shannonsimoncherry@gmail.com>

--- models/trip_event.py
+++ models/trip_event.py
@@ -1,5 +1,8 @@
 from models.base_model import BaseModel
+import peewee as pw
+from models.trip import Trip
 
 class TripEvent(BaseModel):
-    pass
-    # Trip Event Logic - Copy in from Shannon's work files
+    parent_trip_id = pw.ForeignKeyField(Trip, backref='tripevent', unique=False, on_delete='CASCADE')
+    date_time = pw.DateTimeField()
+    location = pw.CharField(unique=False)
