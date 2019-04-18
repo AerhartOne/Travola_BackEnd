7df55181dec112b3311eed622674caa6765c63ff
AerhartOne <AerhartOne@gmail.com>
2019-04-18 13:50:11 +0800

    - Added description to TripEvents

--- models/trip_event.py
+++ models/trip_event.py
@@ -4,14 +4,16 @@ from models.trip import Trip
 
 class TripEvent(BaseModel):
     parent_trip = pw.ForeignKeyField(Trip, backref='trip_events', unique=False, on_delete='CASCADE')
-    date_time = pw.DateTimeField()
-    location = pw.CharField(unique=False)
+    date_time = pw.DateTimeField(unique=False, null=False)
+    location = pw.CharField(unique=False, null=True)
+    desc = pw.TextField(unique=False, null=True)
 
     def as_dict(self):
         json_dict = {
             'id': self.id,
             'parent_trip': self.parent_trip.id,
             'date_time': self.date_time,
-            'location': self.location
+            'location': self.location,
+            'desc': self.desc
         }
         return json_dict
